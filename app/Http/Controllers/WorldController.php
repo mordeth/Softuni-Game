d35ae79e69d884756147ef863a93179b5ea70ea0
Cvetan <cvetanov@gmail.com>
2016-12-05 23:31:04 +0200

    Change home controller

--- app/Http/Controllers/WorldController.php
+++ app/Http/Controllers/WorldController.php
@@ -8,5 +8,23 @@ use App\Http\Requests;
 
 class WorldController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
 }
