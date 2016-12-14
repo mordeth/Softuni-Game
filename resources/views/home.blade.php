46b7b0bcd964f0ac12c0797885c9f5d094506ae8
Cvetan <cvetanov@gmail.com>
2016-12-14 18:28:29 +0200

    Improve main views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,27 +4,21 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    <table class="world-map">
-                    @for ($y = 1; $y < 8; $y++)
-                        <tr>
-                            @for ($x = 1; $x < 11; $x++)
-                                <td>
-                                    @foreach ($castles as $castle)
-                                        @if ($castle->location_x === $x && $castle->location_y === $y)
-                                            @include('world/castle')
-                                        @endif
-                                    @endforeach
-                                </td>
-                            @endfor
-                        </tr>
+            <table class="world-map">
+            @for ($y = 1; $y < 8; $y++)
+                <tr>
+                    @for ($x = 1; $x < 11; $x++)
+                        <td>
+                            @foreach ($castles as $castle)
+                                @if ($castle->location_x === $x && $castle->location_y === $y)
+                                    @include('world/castle')
+                                @endif
+                            @endforeach
+                        </td>
                     @endfor
-                    </table>
-                </div>
-            </div>
+                </tr>
+            @endfor
+            </table>
         </div>
     </div>
 </div>
