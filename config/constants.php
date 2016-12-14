34c84621b52e151e3ee83ce9f57f803d92f333f6
Cvetan <cvetanov@gmail.com>
2016-12-14 23:09:29 +0200

    Add config constants

---
+++ config/constants.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tsvetan
+ * Date: 14.12.2016 г.
+ * Time: 22:52
+ */
+
+return [
+    // Percent resources needed per building level
+    'building_per_level' => 0.20,
+    // Time needed for army to cross a square meter
+    'square_time' => 120,
+    // Time resources are updated
+    'resource_time' => 120,
+
+];
