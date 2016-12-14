0a7bcedd7165175ae09f2a93d1a7772c359604f3
Cvetan <cvetanov@gmail.com>
2016-12-14 18:28:02 +0200

    Add buildings seeder

---
+++ database/seeds/BuildingsTableSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BuildingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('buildings')->insert([
+            'type' => 'castle',
+            'name' => 'Castle',
+            'updateable' => false,
+            'position_x' => 3,
+            'position_y' => 3
+        ]);
+
+        DB::table('buildings')->insert([
+            'type' => 'farm',
+            'name' => 'Farm',
+            'wood_needed' => 400,
+            'stone_needed' => 200,
+            'time_required' => 90,
+            'position_x' => 2,
+            'position_y' => 4
+        ]);
+
+        DB::table('buildings')->insert([
+            'type' => 'mine',
+            'name' => 'Mine',
+            'wood_needed' => 500,
+            'stone_needed' => 100,
+            'time_required' => 180,
+            'position_x' => 4,
+            'position_y' => 4
+        ]);
+
+        DB::table('buildings')->insert([
+            'type' => 'wood',
+            'name' => 'Wood Factory',
+            'wood_needed' => 150,
+            'stone_needed' => 0,
+            'time_required' => 70,
+            'position_x' => 2,
+            'position_y' => 2
+        ]);
+
+        DB::table('buildings')->insert([
+            'type' => 'stone',
+            'name' => 'Stone Factory',
+            'wood_needed' => 300,
+            'stone_needed' => 100,
+            'time_required' => 90,
+            'position_x' => 4,
+            'position_y' => 2
+        ]);
+    }
+}
