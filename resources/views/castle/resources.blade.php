d986cf1c2180fccd42312baad387bcf129aa7ea8
Cvetan <cvetanov@gmail.com>
2016-12-14 23:11:52 +0200

    Fix typo

--- resources/views/castle/resources.blade.php
+++ resources/views/castle/resources.blade.php
@@ -1,4 +1,4 @@
 <div class="requirements">
 	<span>Wood: {{$building->wood_needed + ($building->wood_needed * ($building->building_level * Config::get('constants.building_per_level')))}} / {{$properties['resources']->wood}}</span>
-	<span>Stone: {{$building->stone_needed + ($building->stone_needed * ($building->stone_needed * Config::get('constants.building_per_level')))}} / {{$properties['resources']->stone}}</span>
+	<span>Stone: {{$building->stone_needed + ($building->stone_needed * ($building->building_level * Config::get('constants.building_per_level')))}} / {{$properties['resources']->stone}}</span>
 </div>
