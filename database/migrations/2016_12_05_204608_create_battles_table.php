7aff38e373fd199a8d4ccaf4d2b0de3183c3d85f
Cvetan <cvetanov@gmail.com>
2016-12-17 11:48:16 +0200

    Add fields to battles table

--- database/migrations/2016_12_05_204608_create_battles_table.php
+++ database/migrations/2016_12_05_204608_create_battles_table.php
@@ -14,6 +14,17 @@ class CreateBattlesTable extends Migration
     {
         Schema::create('battles', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('attacker');
+            $table->integer('defender');
+            $table->string("in_progress");
+            $table->integer('winner');
+            $table->timestamp('end_time')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->integer("attacker_archer");
+            $table->integer("attacker_swordsman");
+            $table->integer("attacker_knight");
+            $table->integer("defender_archer");
+            $table->integer("defender_swordsman");
+            $table->integer("defender_knight");
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
